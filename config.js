696aa2c72171b3a3465bf5cb35ca5d3d681a9de6
Angelicious24 <angelicameyer98@gmail.com>
2025-07-10 14:42:53 +0200

    First Commit
    09/06/2025

---
+++ config.js
@@ -0,0 +1,11 @@
+
+
+let rootPath = "appacademy@itvarsity.org";
+let apiKey = checkApiKey();
+
+function checkApiKey() {
+    if (!localStorage.getItem("apiKey")) {
+        window.open("enter-api-key.html", "_self");
+    }
+    return localStorage.getItem("apiKey");
+}
